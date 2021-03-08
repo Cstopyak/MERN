0c7d5f5ea198ab78e728d4a71a868000043f906d
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-03-07 18:12:28 -0600

    contact done

--- my-portfolio/client/src/components/Contact.js
+++ my-portfolio/client/src/components/Contact.js
@@ -1,10 +1,78 @@
 import React, { Component } from 'react'
-
+import "../css/Contact.css";
+import { Grid, Cell, Button } from 'react-mdl';
 class Contact extends Component {
     render() {
         return (
-            <div>
-                    <h1> Contact page</h1>
+            <div className="Contact__Container">
+                {/* <div className="Contact__Form">
+                    <h2 className="Title"> Get In Touch</h2>
+                    <div className="Contact__Center">
+                        <div className="Contact__Form">
+                            <form>
+                                <p> message</p>
+
+                                <label htmlFor="name"> Name</label>
+                                <input type="text" placeholder="import name..."
+                                    required
+                                />
+
+                                <label htmlFor="email"> Email</label>
+                                <input type="text" placeholder="import email..."
+                                    required
+                                />
+
+                                <label htmlFor="message"> Message</label>
+                                <textarea type="text" name="message" id="" placeholder='import contact reason...' cols="30"
+                                    rows="10"></textarea>
+
+                                <div className="send-btn">
+                                    <button type="submit">Send Email</button>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div> */}
+                <div className="Contact">
+                    <Grid className="Contact__Grid">
+                        <Cell col={12}>
+
+                            <div className="Banner">
+                                <h1> Lets Get In Touch</h1>
+                                <hr />
+                            </div>
+                            <form>
+
+                                <div className="Form">
+                                    <div className="Form__Control">
+                                        <label className="Label" htmlFor="name"> Name</label>
+                                        <input type="text" placeholder="name..."
+                                            required
+                                        />
+                                    </div>
+                                    <div className="Form__Control">
+                                        <label className="Label" htmlFor="email"> Email</label>
+                                        <input type="text" placeholder="email..."
+                                            required
+                                        />
+                                    </div>
+                                    <div className="Form__Control">
+                                        <label className="Label" htmlFor="message"> Message</label>
+                                    </div>
+                                    <div className="Form__Control">
+                                        <textarea type="text" name="message" id="" placeholder='contact reason...' cols="30"
+                                            rows="10"></textarea>
+                                    </div>
+                                    <div className="send-btn">
+                                        <Button raised colored className="Button__style" type="submit">Send Email</Button>
+                                    </div>
+                                </div>
+                                
+                            </form>
+                        </Cell>
+                    </Grid>
+                </div>
             </div>
         )
     }
--- my-portfolio/server/routes/ContactRoute.js
+++ my-portfolio/server/routes/ContactRoute.js
@@ -1,27 +1,27 @@
 const router = require('express').Router();
 const nodemailer = require('nodemailer');
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.send('contacted')
 })
 
-router.post('/', (req, res) =>{
+router.post('/', (req, res) => {
     //setup for transporting messages or email
     let smtpTransport = nodemailer.createTransport({
         service: 'Gmail',
         //port to connect
-        port:465,
+        port: 465,
         //authentication
         auth: {
             user: 'colby.stopyak@gmail.com',
             pass: 'googlecolby21'
         }
     })
-let mailOptions = {
-    from: data.email,
-    to: 'colby.stopyak@gmail.com',
-    subject: `Message from ${data.name}`,
-    html: ` <h3> Information </h3>
+    let mailOptions = {
+        from: data.email,
+        to: 'colby.stopyak@gmail.com',
+        subject: `Message from ${data.name}`,
+        html: ` <h3> Information </h3>
     <ul> 
         <li> Name: ${data.name}</li>
         <li> Name: ${data.email}</li>
@@ -29,12 +29,20 @@ let mailOptions = {
     <h3> Message </h3>
     <p>${data.message} </p>
     `
-}
+    }
 
 
-smtpTransport.sendMail(mailOptions, (err,res) =>{
+    smtpTransport.sendMail(mailOptions, (err, res) => {
+        try {
+            if (err) return res.status(400).json({ msg: err })
+            else {
+                return res.status(200).json({ msg: `message was sent` })
+            }
+        } catch (err) {
+            res.status(500).json({ msg: err })
+        }
 
-})
+    })
 
 
 
