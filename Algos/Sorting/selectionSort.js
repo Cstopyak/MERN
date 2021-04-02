b01d971747b75d24c299f2699a0fc85c32aea1f7
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-04-02 08:27:17 -0500

    updates on selection sort

--- Algos/Sorting/selectionSort.js
+++ Algos/Sorting/selectionSort.js
@@ -1,17 +1,17 @@
-// insertion sort 
+// Selection sort 
 //Sorted
 // |  5-1 is unsorted
 //[9,5,2,7,1]
 
-//Insertion Sort
+//Selection Sort
 // Given an array of numbers, sort the numbers by placing values into their correct position at the front (sorted part) of the array
 // Ex:
 // [9,5,2,7,1] -> [5,9,2,7,1] -> [2,5,9,7,1] -> [2,5,7,9,1] -> [1,2,5,7,9]
  array = [9, 5, 2, 7, 1]
-function insertionSort(array) {
+function selectionSort(array) {
     for (let i = 1; i < array.length; i++) {
         let x = i;
-        while (x > 0 && array[x] < array[x - 1]) {  //havnt current num trying to insert is greater than 0, and current  num out of position/ eventually going to loop backwords with current number
+        while (x > 0 && array[x] < array[x - 1]) {  //havnt current num trying to Selection is greater than 0, and current  num out of position/ eventually going to loop backwords with current number
             swap(x, x - 1, array);
             x -= 1;
         }
@@ -27,11 +27,11 @@ function swap(i, x, array) {
     array[x] = array[i];
     array[i] = temp;
 }
-console.log(insertionSort([9, 5, 2, 7, 1]))
+console.log(selectionSort([9, 5, 2, 7, 1]))
 
 // ex 2
 
-function insert(arr){
+function Selection(arr){
     var temp 
     var index = 1
     for(var i = 0; i< arr.length; i++){
@@ -48,4 +48,4 @@ function insert(arr){
     }
     return arr
 }
-console.log(insert([9,5,2,7,1]))
+console.log(Selection([9,5,2,7,1]))
